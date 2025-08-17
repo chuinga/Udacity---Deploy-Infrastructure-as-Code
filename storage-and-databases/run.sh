3c09414d288aada00b8b773edad72956bf7424d1
miguelMartinsDeveloper <miguel.martins@tui.com>
2025-08-17 19:51:42 +0100

    5.14

--- storage-and-databases/run.sh
+++ storage-and-databases/run.sh
@@ -2,12 +2,16 @@
 # Automation script for CloudFormation templates. 
 #
 # Parameters
-#   $1: Execution mode. Valid values: deploy, delete, preview.
+#   $1: An execution mode argument for executing either a deploy, delete or preview workflow. The script must validate it receives accepted values for this argument.
+#   $2: The target AWS region.
+#   $3: The name of the target CloudFormation stack.
+#   $4: The name of your template file (optional parameter).
+#   $5: The name of your parameters file (optional parameter).
 #
 # Usage examples:
-#   ./run.sh deploy 
-#   ./run.sh preview
-#   ./run.sh delete
+#   ./run.sh deploy us-east-1 udacity-scripts-exercise exercise.yml exercise-params.json
+#   ./run.sh preview us-east-1 udacity-scripts-exercise exercise.yml exercise-params.json
+#   ./run.sh delete us-east-1 udacity-scripts-exercise
 #
 
 # Validate parameters
@@ -16,22 +20,33 @@ if [[ $1 != "deploy" && $1 != "delete" && $1 != "preview" ]]; then
     exit 1
 fi
 
+EXECUTION_MODE=$1
+REGION=$2
+STACK_NAME=$3
+TEMPLATE_FILE_NAME=$4
+PARAMETERS_FILE_NAME=$5
+
 # Execute CloudFormation CLI
-if [ $1 == "deploy" ]
+if [ $EXECUTION_MODE == "deploy" ]
 then
     aws cloudformation deploy \
-        --stack-name udacity-parametrizing-scripts-lesson \
-        --template-file vpc.yml
+        --stack-name $STACK_NAME \
+        --template-file $TEMPLATE_FILE_NAME \
+        --parameter-overrides file://$PARAMETERS_FILE_NAME \
+        --region=$REGION
 fi
-if [ $1 == "delete" ]
+if [ $EXECUTION_MODE == "delete" ]
 then
     aws cloudformation delete-stack \
-        --stack-name udacity-parametrizing-scripts-lesson \
+        --stack-name $STACK_NAME \
+        --region=$REGION
 fi
-if [ $1 == "preview" ]
+if [ $EXECUTION_MODE == "preview" ]
 then
     aws cloudformation deploy \
-        --stack-name udacity-parametrizing-scripts-lesson \
-        --template-file vpc.yml \
-        --no-execute-changeset
+        --stack-name $STACK_NAME \
+        --template-file $TEMPLATE_FILE_NAME \
+        --parameter-overrides file://$PARAMETERS_FILE_NAME \
+        --no-execute-changeset \
+        --region=$REGION 
 fi
